c892220e1123936b4a6c4ace2df196ae8fce3ea0
Stroganov Ivan <gangun.24816@yandex.ru>
2024-02-08 18:36:42 +0300

    .

--- sem1/Program.cs
+++ sem1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// int firstNumber = 25;
+// int secondNumber = 5;
+// if (secondNumber * secondNumber == firstNumber)
+// {
+//     Console.WriteLine("Да");
+// }
+// else
+// {
+//     Console.WriteLine("Нет");
+// }
+
+// int N = 4;
+// int neganiveN = -N;
+// while (neganiveN <= N)
+// {
+//     Console.Write(neganiveN + " ");
+//     neganiveN++;
+// }
+
+// int number = 435;
+// int firstDigit = number / 100;
+// int thirdDigit = number % 10;
+// int res = firstDigit + thirdDigit;
+// System.Console.WriteLine("Результат: " + res);
---
+++ sem2/Program.cs
@@ -0,0 +1,17 @@
+﻿// int number = 971;
+// int result = number / 100 * 10 + number % 10;
+// Console.WriteLine(result);
+
+// int number = 972;
+// int secondDigit = number % 100 / 10;
+// int thirdDigit = number % 10;
+// int result = (int)Math.Pow(secondDigit, thirdDigit);
+// Console.WriteLine($"{secondDigit} в степени {thirdDigit} равно {result}");
+
+// int number = 9012;
+// if (number < 100) {Console.WriteLine("третьей цифры нет");}
+// else
+// {
+//     int result = number / 100 % 10;
+//     Console.WriteLine(result);
+// }
