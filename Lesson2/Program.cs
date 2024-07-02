b24d7d90ca2b80b28be3f0c9bd5bd701c117ac56
Stroganov Ivan <gangun.24816@yandex.ru>
2024-07-02 04:23:42 +0300

    Create Lesson2

---
+++ Lesson2/Program.cs
@@ -0,0 +1,27 @@
+﻿//№1
+// Console.Write("Введите трёхзначное число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if (number > 99 && number < 1000)
+// {
+//     int result = number / 100 * 10 + number % 10;
+//     Console.WriteLine(result);
+// }
+// else {Console.WriteLine("Вы ввели некорректное число.");}
+
+
+//№2
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99 && number < 1000)
+{
+    int digit = number / 10 % 10;
+    int result = 1;
+    for (int power = number % 10; power > 0; power--)
+    {
+        result *= digit;
+    }    
+    Console.WriteLine(result);
+}
+else {Console.WriteLine("Вы ввели некорректное число.");}
