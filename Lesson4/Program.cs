a3cc7f74620120637769a3acfe7244d649dbbfc7
Stroganov Ivan <gangun.4444@gmail.com>
2024-09-16 23:19:46 +0300

    Не знаю

---
+++ Lesson4/Program.cs
@@ -0,0 +1,33 @@
+﻿void Main()
+{
+int arraySize = ReadInt("Введите размер массива: ");
+int[] array = GenerateArray(arraySize, 0, 10);
+PrintArray(array);
+}
+
+void PrintArray(int[] arrayForPrint)
+{
+for(int i = 0; i < arrayForPrint.Length; i++)
+System.Console.Write(arrayForPrint[i] + " ");
+
+System.Console.WriteLine();
+}
+
+int[] GenerateArray(int size, int minValue, int maxValue)
+{
+int[] tempArray = new int[size];
+Random rand = new Random();
+
+for (int i = 0; i < tempArray.Length; i++)
+tempArray[i] = rand.Next(minValue, maxValue);
+
+return tempArray;
+}
+
+int ReadInt(string msg)
+{
+System.Console.Write(msg);
+return Convert.ToInt32(Console.ReadLine()); // 13
+}
+
+Main();
